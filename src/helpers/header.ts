51e3dcbc1bf23d81efd0e81f7c3f9982b8cca39f
Ryan Lin <601882460@qq.com>
2019-11-06 18:33:21 +0800

    feat(src and example): 基本功能完成
    BREAKING CHANGE: n

---
+++ src/helpers/header.ts
@@ -0,0 +1,46 @@
+import { isPlainObject } from './uitl'
+
+const CONTENT_TYPE = 'Content-Type'
+
+function normalizeHeaderName(headers: any, normalizeName: string): void {
+  if (!headers) {
+    return
+  }
+
+  Object.keys(headers).forEach(name => {
+    if (name !== normalizeName && name.toUpperCase() === normalizeName.toUpperCase()) {
+      headers[normalizeName] = headers[name]
+      delete headers[name]
+    }
+  })
+}
+
+export function processHeaders(headers: any, data: any): any {
+  normalizeHeaderName(headers, CONTENT_TYPE)
+
+  if (isPlainObject(data)) {
+    if (headers && !headers[CONTENT_TYPE]) {
+      headers[CONTENT_TYPE] = 'application/json;charset=utf-8'
+    }
+  }
+  return headers
+}
+
+export function parseHeaders(headers: string): any {
+  let parsed = Object.create(null)
+  if (!headers) {
+    return parsed
+  }
+
+  headers.split('\r\n').forEach(line => {
+    let [key, val] = line.split(':')
+    key = key.trim().toLowerCase()
+    if (!key) return
+    if (val) {
+      val = val.trim()
+    }
+    parsed[key] = val
+  })
+
+  return parsed
+}
